29eddbfbce58fe078cc449772563ce945804b304
Pavel Rid <81824640+s0ulw1sh@users.noreply.github.com>
2021-07-25 09:37:29 +0300

    Update main.c

--- 191.Number of 1 Bits/main.c
+++ 191.Number of 1 Bits/main.c
@@ -1,13 +1,12 @@
-#define count_bit(byte)       \
-    ((byte & 0x01 ? 1 : 0) + \
-    (byte & 0x02 ? 1 : 0)  + \
-    (byte & 0x04 ? 1 : 0)  + \
-    (byte & 0x08 ? 1 : 0)  + \
-    (byte & 0x10 ? 1 : 0)  + \
-    (byte & 0x20 ? 1 : 0)  + \
-    (byte & 0x40 ? 1 : 0)  + \
-    (byte & 0x80 ? 1 : 0))
-
+#define count_bit(byte)      \
+    (((byte & 0x01) != 0)  + \
+    ((byte & 0x02)  != 0)  + \
+    ((byte & 0x04)  != 0)  + \
+    ((byte & 0x08)  != 0)  + \
+    ((byte & 0x10)  != 0)  + \
+    ((byte & 0x20)  != 0)  + \
+    ((byte & 0x40)  != 0)  + \
+    ((byte & 0x80)  != 0))
 
 int hammingWeight(uint32_t n) {
     int r = 0;
