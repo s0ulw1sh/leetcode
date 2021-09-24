632c5198e93e3543c45649d38200da5c4fe52018
Pavel Rid <81824640+s0ulw1sh@users.noreply.github.com>
2021-09-24 18:49:00 +0300

    Update main.c

--- 2. Add Two Numbers/main.c
+++ 2. Add Two Numbers/main.c
@@ -6,12 +6,12 @@
  * };
  */
 
-struct ListNode* newListNode(struct ListNode *from)
+struct ListNode* newListNode(struct ListNode *from, int val)
 {
     struct ListNode *node;
     
     node       = (struct ListNode*) malloc(sizeof(struct ListNode));
-    node->val  = 0;
+    node->val  = val;
     node->next = NULL;
     
     if (from) {
@@ -22,38 +22,31 @@ struct ListNode* newListNode(struct ListNode *from)
 }
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-    int l1val,
-        l2val,
-        i = 0, acm;
+    int sum = 0;
     struct ListNode *el, *res;
     
-    el  = newListNode(NULL);
-    res = el;
+    sum = l1->val + l2->val;
     
-    l1val = l1->val;
-    l2val = l2->val;
-
-    while(l1 || l2) {
-        acm = l1val + l2val + i;
-        
-        el->val = acm % 10;
-        i       = acm / 10;
-        
-        l1 = l1 ? l1->next : NULL;
-        l2 = l2 ? l2->next : NULL;
-        
-        if (l1 || l2) {
-            el = newListNode(el);
-
-            l1val = l1 ? l1->val : 0;
-            l2val = l2 ? l2->val : 0;
+    el   = newListNode(NULL, sum % 10);
+    res  = el;
+    l1   = l1->next;
+    l2   = l2->next;
+    sum /= 10;
+    
+    while(l1 || l2 || sum) {
+        if (l1) {
+            sum += l1->val;
+            l1   = l1->next;
         }
+        
+        if (l2) {
+            sum += l2->val;
+            l2   = l2->next;
+        }
+        
+        el   = newListNode(el, sum % 10);
+        sum /= 10;
     }
     
-    if (i) {
-        el = newListNode(el);
-        el->val = i;
-    }
-
     return res;
 }
