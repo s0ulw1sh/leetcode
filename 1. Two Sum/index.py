1ac4e5e4f2bc053e710f575dca6162e9acdc67c9
Pavel Rid <81824640+s0ulw1sh@users.noreply.github.com>
2021-09-28 00:38:43 +0300

    Create index.py

---
+++ 1. Two Sum/index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        out = [-1, -1]
+        obj = {}
+        
+        for i in range(len(nums)):
+            obj[nums[i]] = i
+        
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            
+            if diff in obj and i != obj[diff]:
+                out[0] = i
+                out[1] = obj[diff]
+                
+                return out
+        
+        return out
